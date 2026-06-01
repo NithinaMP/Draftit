0855ff2338aab7df060bc85d70e96f6b74a1ee63
NithinaMP <nithinampnithu@gmail.com>
2026-06-01 15:02:45 +0530

    Build provider architecture for exam preparation features

--- lib/features/3_career_builder/data/local/hive_job_box.dart
+++ lib/features/3_career_builder/data/local/hive_job_box.dart
@@ -1,46 +1,3 @@
-// import 'package:hive_flutter/hive_flutter.dart';
-// import '../models/job_application_model.dart';
-//
-// class HiveJobBox {
-//   static const _boxName = 'job_applications_box';
-//   Box<JobApplication>? _box;
-//
-//   Future<Box<JobApplication>> get box async {
-//     _box ??= await Hive.openBox<JobApplication>(_boxName);
-//     return _box!;
-//   }
-//
-//   Future<void> save(JobApplication app) async {
-//     final b = await box;
-//     await b.put(app.id, app);
-//   }
-//
-//   Future<List<JobApplication>> getAll() async {
-//     final b = await box;
-//     return b.values.toList()
-//       ..sort((a, b) => b.createdAt.compareTo(a.createdAt));
-//   }
-//
-//   Future<JobApplication?> get(String id) async {
-//     final b = await box;
-//     return b.get(id);
-//   }
-//
-//   Future<void> updateStatus(String id, String status) async {
-//     final b = await box;
-//     final app = b.get(id);
-//     if (app != null) {
-//       app.status = status;
-//       await app.save();
-//     }
-//   }
-//
-//   Future<void> delete(String id) async {
-//     final b = await box;
-//     await b.delete(id);
-//   }
-// }
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import '../models/job_application_model.dart';
@@ -90,8 +47,3 @@ class HiveJobBox {
 
   void reset() => _box = null;
 }
-
-
-
-
-
